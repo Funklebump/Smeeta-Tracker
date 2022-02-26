118321ce18327d18cc6b69d38a4a22f1f04966d9
A-DYB <adrian.dybka@mail.mcgill.ca>
2022-02-26 18:19:04 -0500

    fix updater

--- updater.py
+++ updater.py
@@ -3,4 +3,7 @@ import os
 
 repo_dir = os.path.join(os.getcwd())
 repo = git.Repo.init(repo_dir)
+repo.git.add('*')
+#repo.git.stash()
+#repo.git.reset('--hard')
 git.cmd.Git().pull('https://github.com/A-DYB/smeeta-tracker-2' , 'main')
