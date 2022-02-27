abaa6e862d01dfde8478ff3eea9dca83d1422944
A-DYB <adrian.dybka@mail.mcgill.ca>
2022-02-26 19:03:19 -0500

    'test'

--- updater.py
+++ updater.py
@@ -4,9 +4,9 @@ import subprocess
 
 repo_dir = os.path.join(os.getcwd())
 repo = git.Repo.init(repo_dir)
-repo.git.add('*')
+#repo.git.add('*')
 #repo.git.stash()
-#repo.git.reset('--hard')
+repo.git.reset('--hard HEAD')
 git.cmd.Git().pull('https://github.com/A-DYB/smeeta-tracker-2' , 'main')
 
 subprocess.Popen(['python', './main.py'])
